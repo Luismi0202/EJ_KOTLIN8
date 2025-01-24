22902045b12bd981be833ce8a2a69ca94e85bb5e
lgomdom0202 <lgomdom0202@g.educaand.es>
2025-01-24 16:41:50 +0100

    Modificaciones hechas para que hayan algunos require

--- Libro.kt
+++ Libro.kt
@@ -4,6 +4,12 @@ class Libro(
     var numPaginas: Int,
     var calificacion: Int
 ) {
+    init{
+        require(calificacion in 0..10){"La calificación tiene que estar en el rango de 0 y 10!"}
+        require(numPaginas > 0){"¡Un libro no puede tener 0 páginas!"}
+        require(titulo.trim().isNotEmpty()){"¡Un libro tiene que tener un título!"}
+        if(autor.trim().isEmpty()) autor = "Anónimo"
+    }
     override fun toString(): String {
         return "Titulo: $titulo autor: $autor numPaginas: $numPaginas calificacion: $calificacion"
     }
